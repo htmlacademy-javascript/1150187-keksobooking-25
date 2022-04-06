bc8b41677425700501c28831a461b83ae1469272
Keks <keks@htmlacademy.ru>
2022-04-06 22:18:08 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/map.js
@@ -0,0 +1,96 @@
+import { deactivateForm, activateForm, addressInput } from './form.js';
+import { getData } from './network.js';
+import { getRandomNumber } from './util.js';
+import { generatePopup } from './popup.js';
+
+deactivateForm();
+
+const startingCoordinates = {
+  lat: 35.68245,
+  lng: 139.76963
+};
+
+const map = L.map('map-canvas')
+  .on('load', () => {
+    activateForm();
+  })
+  .setView({
+    lat: 35.68245,
+    lng: 139.76963,
+  }, 13);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const pinIcon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+const mainPin = L.marker(
+  {
+    lat: 35.68245,
+    lng: 139.76963,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon
+  }
+);
+
+const pinsGroup = L.layerGroup().addTo(map);
+
+mainPin.addTo(pinsGroup);
+addressInput.value = `${mainPin.getLatLng().lat.toFixed(5)}, ${mainPin.getLatLng().lng.toFixed(5)}`;
+
+const createPin = (element) => {
+  const lat = element.location.lat;
+  const lng = element.location.lng;
+
+  const marker = L.marker(
+    {
+      lat,
+      lng
+    },
+    {
+      icon: pinIcon
+    }
+  );
+
+  marker
+    .addTo(pinsGroup)
+    .bindPopup(generatePopup(element));
+};
+
+getData((data) => {
+  const randomNumber = getRandomNumber(0, 39);
+  data.slice(randomNumber, randomNumber + 10).forEach((element) => {
+    createPin(element);
+  });
+});
+
+mainPin.on('move', (evt) => {
+  addressInput.value = `${evt.target.getLatLng().lat.toFixed(5)}, ${evt.target.getLatLng().lng.toFixed(5)}`;
+});
+
+const resetMainPin = () => {
+  const closePopupBtns = document.querySelectorAll('.leaflet-popup-close-button');
+  closePopupBtns.forEach((element) => {
+    element.click();
+  });
+  mainPin.setLatLng(startingCoordinates);
+  addressInput.setAttribute('value', `${startingCoordinates.lat}, ${startingCoordinates.lng}`);
+};
+
+export { resetMainPin };
---
+++ 10/js/network.js
@@ -0,0 +1,82 @@
+const showLoadError = () => {
+  const errorMessage = document.createElement('div');
+  errorMessage.style.padding = '15px';
+  errorMessage.style.width = '100%';
+  errorMessage.style.position = 'fixed';
+  errorMessage.style.top = 0;
+  errorMessage.style.left = 0;
+  errorMessage.style.textAlign = 'center';
+  errorMessage.style.color = '#ffffff';
+  errorMessage.style.backgroundColor = 'red';
+  errorMessage.textContent = 'Не удалось загрузить данные';
+  document.body.append(errorMessage);
+
+  setTimeout(() => {
+    errorMessage.remove();
+  }, 5000);
+};
+
+const showSendError = () => {
+  const errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  const tryAgainBtn = errorTemplate.querySelector('.error__button');
+  const closeErrorMsg = () => {
+    errorTemplate.remove();
+  };
+
+  errorTemplate.addEventListener('click', closeErrorMsg);
+  tryAgainBtn.addEventListener('click', closeErrorMsg);
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      closeErrorMsg();
+    }
+  });
+  document.body.append(errorTemplate);
+};
+
+const showSendSuccessMsg = () => {
+  const successTemplate = document.querySelector('#success').content.querySelector('.success');
+  const closeSendSuccessMsg = () => {
+    successTemplate.remove();
+  };
+
+  successTemplate.addEventListener('click', closeSendSuccessMsg);
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      closeSendSuccessMsg();
+    }
+  });
+
+  document.body.append(successTemplate);
+};
+
+const getData = (onSuccess) =>
+  fetch('https://25.javascript.pages.academy/keksobooking/data')
+    .then((response) => response.json())
+    .then((data) => {
+      onSuccess(data);
+    })
+    .catch(() =>
+      showLoadError()
+    );
+
+const sendData = (onSuccess, onError, body) => {
+  fetch(
+    'https://25.javascript.pages.academy/keksobooking',
+    {
+      method: 'POST',
+      body: body
+    }
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onError('Не удалось отправить форму. Попробуйте ещё раз');
+      }
+    })
+    .catch(() => {
+      onError('Не удалось отправить форму. Попробуйте ещё раз');
+    });
+};
+
+export { getData, sendData, showLoadError, showSendSuccessMsg, showSendError };
